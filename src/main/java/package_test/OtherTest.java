8cf515d14f1a9d91123ddf4af1dc079bf9c38219
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2019-01-14 20:05:10 +0300

    another tests added
    (cherry picked from commit 231c94babf6acf19d13b9255d9dfe1e8df5eb686)

--- src/main/java/package_test/OtherTest.java
+++ src/main/java/package_test/OtherTest.java
@@ -8,4 +8,19 @@ public class OtherTest extends Base {
         run();
     }
 
+    @Test
+    public void testestOK() {
+        positive_test(7 - 5);
+    }
+
+    @Test
+    public void testestNotOK() {
+        positive_test(5 - 5);
+    }
+
+    @Test
+    public void testestAnotherNotOK() {
+        positive_test(5 - 7);
+    }
+
 }
